62130b806855fabe83797167b1d331bdca42389d
melik-droid <melikcaymaz@gmail.com>
2025-05-31 03:59:43 +0200

    arranged colors and improved navbar and homepage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAccount } from "wagmi";
 
 function Navbar() {
+  const { address, isConnected } = useAccount();
+
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(0, 0, 0, 0.8)] backdrop-blur-lg border-b border-white/50 shadow-lg">
-      <div className="max-w-5xl mx-auto px-4">
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(0, 0, 0, 0.8)] backdrop-blur-lg border-b border-white/50 shadow-lg h-16">
+      <div className="w-full mx-auto px-30">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-4">
             <a
@@ -13,7 +16,7 @@ function Navbar() {
               className="font-mono text-xl font-bold dark:text-white text-black mr-10"
             >
               Chrono
-              <span className="bg-gradient-to-r from-green-500 to-cyan-500 bg-clip-text text-transparent">
+              <span className="bg-gradient-to-r from-indigo-500 to-cyan-500 bg-clip-text text-transparent">
                 Trade
               </span>
             </a>
@@ -21,29 +24,44 @@ function Navbar() {
             <div className="flex items-center space-x-8">
               <Link
                 to="/explore"
-                className="text-gray-300 hover:text-white transition-colors"
+                className="text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white hover:-translate-y-0.5 transition-all duration-200 hover:font-bold"
               >
                 Explore
               </Link>
 
-              <Link
-                to="/"
-                className="text-gray-300 hover:text-white transition-colors"
-              >
-                Dummy
-              </Link>
+              {isConnected && address ? (
+                <Link
+                  to={`/my-services/${address}`}
+                  className="text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white hover:-translate-y-0.5 transition-all duration-200 hover:font-bold"
+                >
+                  My Services
+                </Link>
+              ) : (
+                <span className="text-gray-400 cursor-not-allowed">
+                  My Services
+                </span>
+              )}
 
               <Link
                 to="/"
-                className="text-gray-300 hover:text-white transition-colors"
+                className="text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white hover:-translate-y-0.5 transition-all duration-200 hover:font-bold"
               >
                 Dummy2
               </Link>
             </div>
           </div>
 
-          <div>
+          <div className="flex items-center space-x-4">
             <ConnectButton chainStatus={"none"} accountStatus={"avatar"} />
+            {isConnected && address && (
+              <Link to="/profile" className="mx-2">
+                <img
+                  src="https://i.pravatar.cc/40"
+                  alt="Profile"
+                  className="w-10 h-10 rounded-full border-2 border-white shadow"
+                />
+              </Link>
+            )}
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,38 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { config } from "./config";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  darkTheme,
+  lightTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-    <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-            <RainbowKitProvider>
-                <StrictMode>
-                    <App />
-                </StrictMode>
-            </RainbowKitProvider>
-        </QueryClientProvider>
-    </WagmiProvider>
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider
+        theme={{
+          darkMode: darkTheme({
+            accentColor: "var(--color-primary)",
+            accentColorForeground: "white",
+            fontStack: "system",
+            overlayBlur: "small",
+          }),
+          lightMode: lightTheme({
+            accentColor: "var(--color-primary)",
+            accentColorForeground: "white",
+            fontStack: "system",
+            overlayBlur: "small",
+          }),
+        }}
+      >
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
 );
